1da2107fdfbdbc1c015762d2a0d5342028d687ff
NY64 <me@ny64.kr>
2024-08-10 15:39:45 +0900

    feat: sussy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,5 +3,5 @@ import { useBlocker } from "react-router-dom";
 export default function Home() {
 	useBlocker(({ currentLocation, nextLocation }) => value !== "" && currentLocation.pathname !== nextLocation.pathname);
 
-	return ()
+	return <></>;
 }

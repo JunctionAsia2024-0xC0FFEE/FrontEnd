f3b7914c4bf86360ae8f5f7a8f08ba213b535693
5-23 <yhanbyeol6@gmail.com>
2024-08-11 04:08:54 +0900

    idk

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -15,6 +15,15 @@ fn is_ios() -> bool {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
+        .setup(move |app| {
+            #[cfg(mobile)]
+            {
+                app.handle()
+                    .plugin(tauri_plugin_barcode_scanner::init())
+                    .unwrap();
+            }
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![is_ios])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");

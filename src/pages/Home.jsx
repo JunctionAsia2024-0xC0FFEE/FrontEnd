8eb3282e923e2794eff96eb96de38226cb3ce21f
NY64 <me@ny64.kr>
2024-08-10 15:39:32 +0900

    feat: sussy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,7 @@
 import { useBlocker } from "react-router-dom";
 
 export default function Home() {
-	let blocker = useBlocker(({ currentLocation, nextLocation }) => value !== "" && currentLocation.pathname !== nextLocation.pathname);
-	// Add valid code here
+	useBlocker(({ currentLocation, nextLocation }) => value !== "" && currentLocation.pathname !== nextLocation.pathname);
 
-
-	return (
-			blocker.state === "blocked" ? (
-				<div>
-					<p>Are you sure you want to leave?</p>
-					<button onClick={() => blocker.proceed()}>Proceed</button>
-					<button onClick={() => blocker.reset()}>Cancel</button>
-				</div>
-			) : null;
-	);
+	return ()
 }
